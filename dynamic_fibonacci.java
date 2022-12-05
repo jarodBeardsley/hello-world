22e4e9cbc571ba6a72f7fd5648c998c23f7b5654
jarodBeardsley <jarod.beardsley@gmail.com>
2022-12-05 12:49:44 -0500

    Create dynamic_fibonacci.java

---
+++ dynamic_fibonacci.java
@@ -0,0 +1,17 @@
+int fib(int n) {
+
+     if(n<=1) { 
+           return n;
+     }
+
+     if (fib(n) != -1) {
+           return fib(n);
+     }
+     
+     int res = fib(n-1) + fib(n-2); 
+
+     fib(n) = res;
+
+     return res;
+
+} 
